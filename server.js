faf9bdadfc2cacd3a6dc48bf7f15a85525b6d660
Shari Hobbs <sharirhobbs@gmail.com>
2018-03-23 13:19:07 -0700

    added test file

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express'); 
+const morgan = require('morgan');
+const router = express.Router();
+
+const app = express(); 
+
+const blogPostsRouter = require('./blogPostsRouter');
+
+app.use(morgan('common'));
+app.use(express.static('public')); 
+
+app.use('/blogposts', blogPostsRouter);
+
+let server;
+
+function runServer() {
+  const port = process.env.PORT || 8080;
+  return new Promise((resolve, reject) => {
+    server = app.listen(port, () => {
+      console.log(`Your app is listening on port ${port}`);
+      resolve(server);
+    }).on('error', err => {
+      reject(err)
+    });
+  });
+}
+
+function closeServer() {
+  return new Promise((resolve, reject) => {
+    console.log('Closing server');
+    server.close(err => {
+      if (err) {
+        reject(err);
+        // so we don't also call `resolve()`
+        return;
+      }
+      resolve();
+    });
+  });
+}
+
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+};
+
+module.exports = {app, runServer, closeServer};
