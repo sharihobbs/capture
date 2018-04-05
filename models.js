a80638ad47e46deb7152bec175ced7453069ce37
Shari Hobbs <sharirhobbs@gmail.com>
2018-04-05 13:37:07 -0700

    added public files

--- models.js
+++ models.js
@@ -1,28 +1,28 @@
-'use strict';
+// 'use strict';
 
-const mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
+// const mongoose = require('mongoose');
+// mongoose.Promise = global.Promise;
 
-const postSchema =  mongoose.Schema({
-  image: {type: String, required: true},
-  created: {type: Date, default: Date.now},
-  text: {type: String, required: true}
- });
+// const postSchema =  mongoose.Schema({
+//   image: {type: String, required: true},
+//   created: {type: Date, default: Date.now},
+//   text: {type: String, required: true}
+//  });
 
- // postSchema.virtual('authorString').get(function() {
-  // return `${this.author.firstName}
-  // ${this.author.lastName}`.trim()});
+//  // postSchema.virtual('authorString').get(function() {
+//   // return `${this.author.firstName}
+//   // ${this.author.lastName}`.trim()});
 
-postSchema.methods.serialize = function() {
-  return {
-    id: this._id,
-    image: this.image,
-    created: this.created,
-    text: this.text
-  };
-}
+// postSchema.methods.serialize = function() {
+//   return {
+//     id: this._id,
+//     image: this.image,
+//     created: this.created,
+//     text: this.text
+//   };
+// }
 
-const Post = mongoose.model('Post', postSchema);
+// const Post = mongoose.model('Post', postSchema);
 
-module.exports = {Post};
+// module.exports = {Post};
 
